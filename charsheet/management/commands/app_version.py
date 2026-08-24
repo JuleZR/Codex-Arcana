e69750957779204f89c5fde6ae920fb18edec3f5
Julian <j.roth@kaosnetz.de>
2026-08-24 17:01:54 +0200

    refactor!: Simplify app_version command
    App Version Command
    - Always print the current application version.
    - Remove the optional deployment-file writing path.
    - Remove the related helper import and CLI argument.
    
    BREAKING CHANGE: The app_version command no longer supports the removed deployment-file writing argument.
    
    Overall:
    Simplifies app_version to focus exclusively on reporting the current application version.

--- charsheet/management/commands/app_version.py
+++ charsheet/management/commands/app_version.py
@@ -1,29 +1,18 @@
-"""Print the automatically calculated Codex Arcana version."""
+"""Print the current Codex Arcana application version."""
 
 from django.core.management.base import BaseCommand
 
 from codex_arcana.versioning import (
     get_application_version,
-    write_deployed_version,
 )
 
 
 class Command(BaseCommand):
-    """Make the version available to deployment scripts."""
+    """Make the current version available to deployment scripts."""
 
     help = "Print the current Codex Arcana application version."
 
-    def add_arguments(self, parser):
-        parser.add_argument(
-            "--write-deployment-file",
-            action="store_true",
-            help="Store the Git version for the deployed web process.",
-        )
-
     def handle(self, *args, **options):
-        if options["write_deployment_file"]:
-            version = write_deployed_version()
-        else:
-            version = get_application_version()
-
-        self.stdout.write(version)
+        self.stdout.write(
+            get_application_version()
+        )
